f4f77f5739401f0701469ab440b1b794a1cae547
Alexandr Krylov <t@ruenya.nix>
2025-05-30 18:31:19 +0300

    improve setup.sh

--- .openhands/setup.sh
+++ .openhands/setup.sh
@@ -1,4 +1,4 @@
 #!/bin/env bash
 sudo apt update
-sudo apt install cmake clang pkg-config libsdl2-dev libsdl2-image-dev google-perftools libgtest-dev git 
+sudo apt install -y cmake clang pkg-config libsdl2-dev libsdl2-image-dev google-perftools libgtest-dev git 
 git config --global --add safe.directory /workspace/DendyTanks
