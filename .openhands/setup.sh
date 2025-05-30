03c83d87796c29afb232f55c27abc240107d9de2
Alexandr Krylov <t@ruenya.nix>
2025-05-30 18:00:01 +0300

    improve: setup

--- .openhands/setup.sh
+++ .openhands/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/env bash
 sudo apt update
 sudo apt install cmake clang pkg-config libsdl2-dev libsdl2-image-dev google-perftools libgtest-dev git 
-mkdir -p build
-cd build && cmake .. && make -j8 && make install
+git config --global --add safe.directory /workspace/DendyTanks
